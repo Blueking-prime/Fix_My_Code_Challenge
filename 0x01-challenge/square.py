08e18af8bbe11d237db3b4e35dd8a01727ea28ac
Nnamdi Anyanwu <blueking189@gmail.com>
2022-11-06 23:24:05 +0100

    test 02

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,7 +7,6 @@ class square():
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -22,6 +21,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
